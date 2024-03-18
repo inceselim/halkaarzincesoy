3351e8f826f2f9323e8457fe4b58fc904fc2f8be
inceselim <inceselim91@gmail.com>
2024-03-18 17:38:52 +0300

    18.03.24-firebase analytics removed

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import analytics from '@react-native-firebase/analytics';
-import { firebase } from '@react-native-firebase/analytics';
 // ...
 
 export default function HomeScreen() {
-    async () => {
-        const appInstanceId = await analytics().getAppInstanceId();
-        await analytics().setAnalyticsCollectionEnabled(true);
+    // async () => {
+    //     const appInstanceId = await analytics().getAppInstanceId();
+    //     await analytics().setAnalyticsCollectionEnabled(true);
 
-        await firebase.analytics().setConsent({
-            analytics_storage: true,
-            ad_storage: true,
-            ad_user_data: true,
-            ad_personalization: true,
-        });
-    }
+    //     await firebase.analytics().setConsent({
+    //         analytics_storage: true,
+    //         ad_storage: true,
+    //         ad_user_data: true,
+    //         ad_personalization: true,
+    //     });
+    // }
     return (
         <View>
             <Text></Text>
