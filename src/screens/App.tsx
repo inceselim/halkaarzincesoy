7cc040f3ad22c848bdf11dde4537b259a11e8d7b
inceselim <inceselim91@gmail.com>
2024-03-18 11:53:05 +0300

    18.03.24-firebase messaging & analytics

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainNavigation from '../navigation/MainNavigation';
+import messaging from '@react-native-firebase/messaging';
+import { PermissionsAndroid, Platform } from 'react-native';
 
 export default function App() {
+  async function requestUserPermission() {
+    if (Platform.OS == "ios") {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    }
+    else {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    }
+  }
+  useEffect(() => {
+    requestUserPermission()
+  }, [])
   return (
     <MainNavigation />
   );
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import analytics from '@react-native-firebase/analytics';
+import { firebase } from '@react-native-firebase/analytics';
+// ...
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text></Text>
-     </View>
-  );
+    async () => {
+        const appInstanceId = await analytics().getAppInstanceId();
+        await analytics().setAnalyticsCollectionEnabled(true);
+
+        await firebase.analytics().setConsent({
+            analytics_storage: true,
+            ad_storage: true,
+            ad_user_data: true,
+            ad_personalization: true,
+        });
+    }
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
 }
