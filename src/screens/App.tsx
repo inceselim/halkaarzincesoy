56495c399d4e3f6346876433a24d3466279ccb4c
inceselim <inceselim91@gmail.com>
2024-03-19 17:38:40 +0300

    19.03.24-tracking & ads

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import MainNavigation from '../navigation/MainNavigation';
 import messaging from '@react-native-firebase/messaging';
-import { PermissionsAndroid, Platform } from 'react-native';
+import { Alert, PermissionsAndroid, Platform } from 'react-native';
+import { TrackingStatus, requestTrackingPermission } from 'react-native-tracking-transparency';
 
 export default function App() {
   async function requestUserPermission() {
@@ -21,6 +22,40 @@ export default function App() {
   }
   useEffect(() => {
     requestUserPermission()
+  }, [])
+
+
+
+  const [trackingStatus, setTrackingStatus] = React.useState<
+    TrackingStatus | '(loading)'
+  >('(loading)');
+
+  // useEffect(() => {
+  //   request()
+  //   getTrackingStatus()
+  //     .then((status: any) => {
+  //       setTrackingStatus(status);
+  //     })
+  //     // .catch((e) => Alert.alert('Error', e?.toString?.() ?? e));
+  // }, []);
+  const request = useCallback(async () => {
+    try {
+      const status = await requestTrackingPermission();
+      setTrackingStatus(status);
+    } catch (e: any) {
+      Alert.alert('Error', e?.toString?.() ?? e);
+    }
+  }, []);
+  async function getTrackingStatus() {
+    const trackingStatus: any = getTrackingStatus();
+    if (trackingStatus === 'authorized' || trackingStatus === 'unavailable') {
+      // enable tracking features
+    }
+  }
+
+  useEffect(() => {
+    request()
+    getTrackingStatus()
   }, [])
   return (
     <MainNavigation />
