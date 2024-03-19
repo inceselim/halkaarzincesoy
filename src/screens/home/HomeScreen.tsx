7b799ce2d214d5392e92e569e5efa7217f59124c
inceselim <inceselim91@gmail.com>
2024-03-19 10:03:41 +0300

    19.03.24-ios app icons

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import { styles } from '../../styles/styles';
 import { CardView } from '../../components/CardView/CardView';
 import { ButtonPrimary } from '../../components/ButtonPrimary/ButtonPrimary';
@@ -20,20 +20,25 @@ export default function HomeScreen() {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
-                <Text style={styles.textTitle}>Son Halka Arzlar</Text>
-                <CardView>
-                    <View style={styles.viewRowsSpaceBetween}>
-                        <Text style={styles.textBig}>Başlama Tarihi:</Text>
-                        <Text style={styles.textBig}>04.03.2024</Text>
-                    </View>
-                    <View style={styles.viewRowsSpaceBetween}>
-                        <Text style={styles.textBig}>Bitiş Tarihi:</Text>
-                        <Text style={styles.textBig}>04.03.2024</Text>
-                    </View>
-                </CardView>
-                <ButtonPrimary text="Halka Arzlar" />
-                <ButtonPrimary text="Borsa Eğitimleri" />
-                <ButtonPrimary text="Teknik Analizler" />
+                <ScrollView>
+                    <Text style={styles.textTitle}>Son Halka Arzlar</Text>
+                    <CardView>
+                        <Text style={styles.textTitle}>HisseKod</Text>
+                        <Text style={styles.textNormal}>HisseAdı</Text>
+                        <Text />
+                        <View style={styles.viewRowsSpaceBetween}>
+                            <Text style={styles.textBig}>Başlama Tarihi:</Text>
+                            <Text style={styles.textBig}>04.03.2024</Text>
+                        </View>
+                        <View style={styles.viewRowsSpaceBetween}>
+                            <Text style={styles.textBig}>Bitiş Tarihi:</Text>
+                            <Text style={styles.textBig}>04.03.2024</Text>
+                        </View>
+                    </CardView>
+                    <ButtonPrimary text="Halka Arzlar" />
+                    <ButtonPrimary text="Borsa Eğitimleri" />
+                    <ButtonPrimary text="Teknik Analizler" />
+                </ScrollView>
             </View>
         </SafeAreaView>
     );
