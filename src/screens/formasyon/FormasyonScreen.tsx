4b7b431421361f3651baeaf73328e2a7fcf2301e
inceselim <inceselim91@gmail.com>
2024-03-20 12:30:34 +0300

    20.03.24-Formasyon++

--- src/screens/formasyon/FormasyonScreen.tsx
+++ src/screens/formasyon/FormasyonScreen.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, SafeAreaView, Image, ScrollView } from 'react-native';
 import { styles } from '../../styles/styles';
 
 export default function FormasyonScreen() {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
-                <Text style={styles.textTitle}>Formasyonlar</Text>
-                <Image source={require("./11.jpeg")} style={{
-                    height: 300,
-                    width: "100%"
-                }}
-                />
+                <ScrollView>
+                    <Text style={styles.textTitle}>Formasyonlar</Text>
+                    <Image source={require("./11.jpeg")} style={{
+                        height: 300,
+                        width: "100%",
+                        resizeMode: "contain"
+                    }} />
+                    <Image source={require("./12.png")} style={{
+                        height: 300,
+                        width: "100%",
+                        resizeMode: "contain"
+                    }} />
+                    <Image source={require("./13.jpeg")} style={{
+                        height: 300,
+                        width: "100%",
+                        resizeMode: "contain"
+                    }} />
+                    <Image source={require("./133.jpeg")} style={{
+                        height: 300,
+                        width: "100%",
+                        resizeMode: "contain"
+                    }} />
+                    <Image source={require("./134.jpeg")} style={{
+                        height: 300,
+                        width: "100%",
+                        resizeMode: "contain"
+                    }} />
+                    <Image source={require("./135.jpeg")} style={{
+                        height: 300,
+                        width: "100%",
+                        resizeMode: "contain"
+                    }} />
+                </ScrollView>
             </View>
         </SafeAreaView>
     );
