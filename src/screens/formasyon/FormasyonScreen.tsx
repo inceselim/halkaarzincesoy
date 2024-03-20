75449809592dc9931138d0a849ccbe6c70e51a29
inceselim <inceselim91@gmail.com>
2024-03-20 09:42:11 +0300

    20.03.24-++

---
+++ src/screens/formasyon/FormasyonScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { styles } from '../../styles/styles';
+
+export default function FormasyonScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Text></Text>
+            </View>
+        </SafeAreaView>
+    );
+}
